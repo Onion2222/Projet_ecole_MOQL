b5e151be9912f57ea745b6f3fdc79018e5cee94e
Onion2222 <arthurperrin.22@gmail.com>
2020-03-20 15:54:45 +0100

    ajout du projet atmel studio UC2

---
+++ attiny85/UC2/UC2/main.c
@@ -0,0 +1,18 @@
+/*
+ * UC2.c
+ *
+ * Created: 20/03/2020 15:53:57
+ * Author : Onion
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
