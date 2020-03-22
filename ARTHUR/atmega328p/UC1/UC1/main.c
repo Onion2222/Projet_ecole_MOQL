96b1741a9819107469eba1a941f10c7bc410312f
Onion2222 <arthurperrin.22@gmail.com>
2020-03-22 12:56:17 +0100

    programme interruption pour clignotement led debug (marche simu)

--- ARTHUR/atmega328p/UC1/UC1/main.c
+++ ARTHUR/atmega328p/UC1/UC1/main.c
@@ -10,8 +10,16 @@
 #define F_CPU 16000000UL
 #define DEBUG_LED (1<<DDC5)
 
+//variable permettant de basser la frequence de l'interruption TIMER0_COMPA (registre 8 bit trop court)
+volatile uint16_t var_clk=1;
+
+
 ISR (TIMER0_COMPA_vect){
-	PORTC^=DEBUG_LED;
+	
+	if(++var_clk>1000){
+		PORTC^=DEBUG_LED;
+		var_clk=1;
+	}
 }
 
 
@@ -28,13 +36,15 @@ int main(void)
 	sei(); //activation interruptions global
 	
 	//init timer interrupt pour led debug
+	//timer 0 (8 bits ""malheureusement"")
+	
 	TCCR0A=0x00; //00 00 -- 00
 	TCCR0B=0x01; //0 0 -- 0 01 (/1024 prescaler)
 	TCNT0=0; // (init) ?
-	OCR0A=200; //valeur à comparer avec TCNT0 pour générer inter
-	TIMSK0=0x02; //----- 010
-	
-	
+	OCR0A=156; //valeur à comparer avec TCNT0 pour générer inter
+	TIMSK0=0x02; //----- 010 gestion de l'interruption generé par timer0 (3 interuptions possibles)
+	//f=Fcryst/(prescal*(OCR0A ou 256))
+	//f=16 000 000 / (1024 * 250) = 100,16Hz => T = 1/100.16 ~ 1ms
 	
     /* Replace with your application code */
     while (1) 
